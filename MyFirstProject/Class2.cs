5f68015fc476c4a6ae868d0eab26539ad97237a9
ashutoshthete <ashutoshthete24@gmail.com>
2022-08-17 14:54:20 +0530

    My2ndCommit

---
+++ MyFirstProject/Class2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConditionalAssignment01
+{
+    class Class2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("New add");
+        }
+    }
+}
